af36b53ce400f71eafc11a5c7a76b2f167a26456
Mike Coletta <coletta.mike@gmail.com>
2021-01-17 17:54:57 -0800

    Fixed time deduction but on question 4.

--- Assets/script.js
+++ Assets/script.js
@@ -32,65 +32,32 @@ var q4Wrong1 = document.querySelector("#q4Wrong1")
 var q4Wrong2 = document.querySelector("#q4Wrong2")
 var q4Wrong3 = document.querySelector("#q4Wrong3")
 
+//End of game buttons
 var submit = document.querySelector("#submit")
 var goBack = document.querySelector("#goBack")
 var clearHighscores = document.querySelector("#clearHighscores")
 var boardInitials = document.querySelector("#boardInitials")
 var boardScore = document.querySelector("#boardScore")
 
+//Start time
 var timeLeft = 60;
 
+var gameFinished = false
 
-//Will need different sections of HTML and hide/show what the user is using at the time. 
 
-//No loop, just each button continues the game to a new piece of hidden HTML.
 
-//write out the HTML (start page, template questions, all done, high scores) Easier to start with HTML/CSS and then go JS.
-
-//JS 
-//when the user clicks the start page button (1. timer starts going down from X 2. move onto first question)
-    //make a timer using setInterval timer is separate from HTML
-    //add event listener on start button
-    //when that button is pushed unhide first question and hide start. (Use .hide and then use display:none)
-//When user clicks any answer button (1. check if question is right or wrong 2. continues to next question)
-    //event listeners for every answer. An array of objects. Each object would have Question, answers, correct answer.
-        //1.Check if answer correct 2. move onto next question
-//When the user finishes all questions OR timer runs out go to All Done page. (submit initials) Game records time left on clock.
-    //form for initials
-    //save current spot on timer SetTimer will have variable that saves value after last question is finished. Timer -- every second. (1000)
-    //when they submit high score saves Initials those initials go to high score page.
-//View initials and high scores. Go back or clear high scores. 
-    //Two buttons: 1. Clear high scores 2. Restart
-
-//hides start menu and shows the first question.
 function hideStart(){
     startScreen.setAttribute("style", "display:none;");
     question1.setAttribute("style", "display:block;");
 };
 
-//this function runs the timer
-function quizTimer() {
-    var timer = setInterval(function () {
-        timeLeft--;
-
-        if (timeLeft <= 0) {
-            clearInterval(timer);
-            timeRunsOut()
-        } 
-        
-        else {
-            countdownClock.textContent = timeLeft;
-        }
-    }, 1000);
-}
-
-//when you click start button you hide the start menue and start the quiz timer.
+//When you click start button you hide the start menue and start the quiz timer.
 startButton.addEventListener("click", function(){
     hideStart();
     quizTimer();
 });
 
-//hides the first question
+//Moves to question 2 after answering question 1
 function hideQ1(){
     question1.setAttribute("style", "display:none;");
     question2.setAttribute("style", "display:block;");
@@ -120,7 +87,7 @@ q1Wrong3.addEventListener("click", function(){
     timeLeft -= 10;
 });
 
-
+//Moves to question 3 after answering question 2
 function hideQ2(){
     question2.setAttribute("style", "display:none;");
     question3.setAttribute("style", "display:block;");
@@ -150,6 +117,7 @@ q2Wrong3.addEventListener("click", function(){
     timeLeft -= 10;
 });
 
+//Moves to question 4 after answering question 3
 function hideQ3() {
     question3.setAttribute("style", "display:none;");
     question4.setAttribute("style", "display:block;");
@@ -179,19 +147,19 @@ q3Wrong3.addEventListener("click", function(){
     timeLeft -= 10;
 });
 
+//Moves to initial page after answering question 4
 function finishQuiz(){
     question4.setAttribute("style", "display:none;");
     initialForm.setAttribute("style", "display:block;");
     userClock.setAttribute("style", "display:none;")
+    gameFinished = true
     cacheScore();
 
     if (timeleft = 0) {
         timeRunsOut()
         cacheScore()
     }
-    else {
-        clearInterval(quizTimer())
-    }
+    
  }
 
  q4Correct.addEventListener("click", function(){
@@ -199,20 +167,22 @@ function finishQuiz(){
  });
 
  q4Wrong1.addEventListener("click", function(){
-     finishQuiz();
      timeLeft -= 10;
+     finishQuiz();
  });
 
  q4Wrong2.addEventListener("click", function(){
-    finishQuiz();
     timeLeft -= 10;
+    finishQuiz();
 });
 
 q4Wrong3.addEventListener("click", function(){
-    finishQuiz();
     timeLeft -= 10;
+    finishQuiz();
 });
 
+
+//Caches and stores score
 function cacheScore() {
     localStorage.setItem("score", timeLeft);
     console.log(timeLeft)
@@ -221,6 +191,7 @@ function cacheScore() {
     console.log(userScore)
 }
 
+//Caches and stores initials.
 submit.addEventListener("click", function (event) {
 
     var initials = document.querySelector("#initials").value;
@@ -232,38 +203,45 @@ submit.addEventListener("click", function (event) {
         initialForm.setAttribute("style", "display:none;");
         highscores.setAttribute("style", "display:block;");
         boardInitials.textContent = (localStorage.getItem("initials") + " -")
-        boardScore.textContent = localStorage.getItem("score")      
+        boardScore.textContent = localStorage.getItem("score")   
     }
 })
 
+//Back button
 goBack.addEventListener("click", function (event) {
     startScreen.setAttribute("style", "display:block;")
     userClock.setAttribute("style", "display:block;")
     highscores.setAttribute("style", "display:none;");
 })
 
+//Clear high scores button
 clearHighscores.addEventListener("click", function (event) {
     boardInitials.textContent = ""
     boardScore.textContent = ""        
 })
 
-//this function runs the timer
+//This function runs the timer
 function quizTimer() {
     var timer = setInterval(function () {
         timeLeft--;
+        console.log(timer)
 
         if (timeLeft <= 0) {
             clearInterval(timer);
-            timeRunsOut()
+            timeRunsOut();
         } 
         
+        //  if (gameFinished = true) {
+        //      clearInterval(timer);
+        //  }
+
         else {
             countdownClock.textContent = timeLeft;
         }
     }, 1000);
 }
 
-//time runs out function
+//Time runs out function
 function timeRunsOut() {
     question1.setAttribute("style", "display:none;");
     question2.setAttribute("style", "display:none;");
@@ -271,3 +249,28 @@ function timeRunsOut() {
     question4.setAttribute("style", "display:none;");
     highscores.setAttribute("style", "display:block;");
 }
+
+// My pseudocode
+
+//Will need different sections of HTML and hide/show what the user is using at the time. 
+
+//No loop, just each button continues the game to a new piece of hidden HTML.
+
+//write out the HTML (start page, template questions, all done, high scores) Easier to start with HTML/CSS and then go JS.
+
+//JS 
+//when the user clicks the start page button (1. timer starts going down from X 2. move onto first question)
+    //make a timer using setInterval timer is separate from HTML
+    //add event listener on start button
+    //when that button is pushed unhide first question and hide start. (Use .hide and then use display:none)
+//When user clicks any answer button (1. check if question is right or wrong 2. continues to next question)
+    //event listeners for every answer. An array of objects. Each object would have Question, answers, correct answer.
+        //1.Check if answer correct 2. move onto next question
+//When the user finishes all questions OR timer runs out go to All Done page. (submit initials) Game records time left on clock.
+    //form for initials
+    //save current spot on timer SetTimer will have variable that saves value after last question is finished. Timer -- every second. (1000)
+    //when they submit high score saves Initials those initials go to high score page.
+//View initials and high scores. Go back or clear high scores. 
+    //Two buttons: 1. Clear high scores 2. Restart
+
+//hides start menu and shows the first question.
